7fd018caa07797eeea95179b3786c3e516feebfe
Antoine07 <antoine.lucsko@gmail.com>
2020-09-21 12:33:42 +0200

    recursive function example

--- part1_introduction/Exercices/foo_recursive.js
+++ part1_introduction/Exercices/foo_recursive.js
@@ -3,9 +3,12 @@ function foo(count = 10){
     if( count == 0 ) return "Stop" ; 
 
     count = count - 1 ;
-    console.log(count);
+    console.log(count); // affiche les différentes valeurs de count à chaque appel de la fonction
 
     // count--;
-    foo();
+    // Il faut retourner la fonction à chaque fois si on veut avoir la valeur stop s'afficher à la fin
+    return foo(count);
 
 }
+
+console.log( foo() );
